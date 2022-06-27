f30bdcd8b2d8066d5c0708600bb1b3548b8b7c1d
skeenp-lls <69334377+skeenp-lls@users.noreply.github.com>
2022-06-27 20:05:27 +1000

    Revert "Update rrc_ext_web_viewer_h2yz7.js"
    This reverts commit e63aabfa426d9c7a9ef3f79b457741d4bd927825.

--- rrc_ext_web_viewer_h2yz7.js
+++ rrc_ext_web_viewer_h2yz7.js
@@ -11,7 +11,12 @@ When copying template, change the following:
  - Fix style elements
 */
 
-
+//Set widget name
+const ctl_id = 'widget_map_large';
+//Set datasource names
+const ds_ids = ['ds_topo', 'ds_img'];
+// Set layer names
+const lyr_ids = ['rrc_h2y_m02_d25', 'rrc_h2y_m02_d30', 'rrc_h2y_m02_d40'];
 // Set debug flag
 const debug = false;
 // Set debug function
@@ -21,7 +26,7 @@ function debug_log(msg) {
 	}
 }
 // Set function to toggle all but selected
-function select_lyr(win, lyr, ctl_id, ds_ids, lyr_ids, target)  {
+function select_lyr(win, lyr) {
 	// Get map views
 	let mvm = win._mapViewManager;
 	let vws = mvm.jimuMapViewGroups[ctl_id].jimuMapViews;
@@ -40,9 +45,6 @@ function select_lyr(win, lyr, ctl_id, ds_ids, lyr_ids, target)  {
 			// Check layer exists and is the target
 			if (vw.jimuLayerViews[lyr_id] && lyr_ids[lyr_idx] == lyr) {
 				v = true;
-				note.style.backgroundColor = 'yellow';
-			} else {
-				note.style.backgroundColor = 'red';
 			}
 			debug_log('RRC Mod: ' + lyr_id + ' visible: ' + v);
 			// Enable/disable layer
@@ -52,44 +54,18 @@ function select_lyr(win, lyr, ctl_id, ds_ids, lyr_ids, target)  {
 	// Report change to console
 	debug_log("RRC Mod: Layer changed to " + lyr);
 }
-
 // Get parent window
 var w = top.window;
 var d = w.document;
-
-//Set widget name
-const map_ctl_id = 'widget_map_large';
-//Set datasource names
-const lyr_ds_ids = ['ds_topo', 'ds_img'];
-// Set layer names
-const map_lyr_ids = ['rrc_h2y_m02_d25', 'rrc_h2y_m02_d30', 'rrc_h2y_m02_d40'];
-
 // Attach events to event buttons
-function attach_layer_events(widgetid, lyr){
+function attach_events(widgetid, lyr){
 	d.querySelector("[data-widgetid="+widgetid+"]").querySelector('a').addEventListener("click", function() {
-		select_lyr(w, lyr, map_ctl_id, lyr_ds_ids, map_lyr_ids);
+		select_lyr(w, lyr);
 	});	
 }
-
 // Attach events
-attach_layer_events('widget_button_d25','rrc_h2y_m02_d25')
-attach_layer_events('widget_button_d30','rrc_h2y_m02_d30')
-attach_layer_events('widget_button_d40','rrc_h2y_m02_d40')
-
-// Set layer names
-const base_lyr_ids = ['base_mask', 'base_mask_src'];
-
-// Attach events to event buttons
-function attach_basemap_events(widgetid, lyr){
-	let t = d.querySelector("[data-widgetid="+widgetid+"]").querySelector('a')
-	t.addEventListener("click", function() {
-		select_lyr(w, lyr, map_ctl_id, lyr_ds_ids, base_lyr_ids, t);
-	});	
-}
-
-// Attach basemap change events
-attach_basemap_events('widget_23','base_mask')
-attach_basemap_events('widget_24','base_mask_src')
-
+attach_events('widget_button_d25','rrc_h2y_m02_d25')
+attach_events('widget_button_d30','rrc_h2y_m02_d30')
+attach_events('widget_button_d40','rrc_h2y_m02_d40')
 // Report load to console
 debug_log('RRC Mod: Init custom button override');
