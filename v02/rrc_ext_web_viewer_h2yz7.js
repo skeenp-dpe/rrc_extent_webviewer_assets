8ac2c1c85cc6fe0d886dce865a3ca905717a80df
skeenp-dpe <69334377+skeenp-dpe@users.noreply.github.com>
2022-10-13 17:15:47 +1100

    Update rrc_ext_web_viewer_h2yz7.js

--- v02/rrc_ext_web_viewer_h2yz7.js
+++ v02/rrc_ext_web_viewer_h2yz7.js
@@ -1,7 +1,7 @@
 //Set widget name
 const map_id = "widget_map_large";
 //Set datasource names
-const ds_id = "ds_topo";
+const ds_id = "dataSource_1";
 // Set layer names
 const lyr_ids = {
   d25: "rrc_h2y_m02_d25",
